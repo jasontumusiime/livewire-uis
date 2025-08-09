f9c0347a2004cdd122f2a2da1ab9e7182204d9ad
Jason Tumusiime <jasongeorgetumusiime@gmail.com>
2025-08-09 13:07:52 +0300

    Add authentication pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,25 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Volt::route('/', 'users.index');
+// Users will be redirected to this route if not logged in
+Volt::route('/login', 'login')->name('login');
+Volt::route('/register', 'register');
+
+// Define the logout
+Route::get('/logout', function () {
+    auth()->logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+
+    return redirect('/');
+});
+
+// Protected routes here
+Route::middleware('auth')->group(function () {
+    Volt::route('/', 'index');
+    Volt::route('/users', 'users.index');
+    // ... more
+});
+// Volt::route('/', 'users.index');
